d90fa39dd1fb72aa4f1e43a93918a48771e2df7f
Raimondas Rimkevičius <github@mekdrop.name>
2018-03-23 11:29:06 +0200

    maybe this would work

--- src/Plugin.php
+++ src/Plugin.php
@@ -8,8 +8,9 @@ use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 use Composer\Script\Event;
 use Composer\Script\ScriptEvents;
+use MatthiasMullie\PathConverter\Converter;
+use Mouf\NodeJsInstaller\Environment;
 use Mouf\NodeJsInstaller\NodeJsInstaller;
-use Mouf\NodeJsInstaller\NodeJsVersionMatcher;
 use Symfony\Component\Process\Process;
 
 /**
@@ -57,41 +58,32 @@ class Plugin implements PluginInterface, EventSubscriberInterface
 
 		if (file_exists($settings['targetDir'])) {
 			$path = $settings['targetDir'];
+			$is_local = true;
 		} else {
 			$nodeJsInstaller = new NodeJsInstaller($io);
 			$path = $nodeJsInstaller->getGlobalInstallPath('npm');
+			$is_local = false;
 		}
+		$this->verboseWrite('Found node path:' . $path, $io);
 
+		$this->verboseWrite('Executing: npm update', $io);
 		$this->exec('npm update', $path, $io);
-		$this->exec('npm install yarn', $path, $io);
-	}
 
-	/**
-	 * Execute command
-	 *
-	 * @param string $cmd		Command to execute
-	 * @param string $path		Path where to run
-	 * @param IOInterface $io	InputOutput interface
-	 */
-	protected function exec($cmd, $path, IOInterface $io) {
-		$process = new Process($cmd, $path);
-		$process->mustRun(function ($type, $buffer) use ($io) {
-			if (Process::ERR === $type) {
-				$io->writeError($buffer);
-			} else {
-				$io->write($buffer);
-			}
-		});
+		$this->verboseWrite('Executing: npm install yarn', $io);
+		$this->exec('npm install yarn', $path, $io);
+
+		$this->createBinScripts($binDir, $path, $is_local);
 	}
 
 	/**
 	 * Gets settings
 	 *
-	 * @param Composer $composer  Current composer instance
+	 * @param Composer $composer Current composer instance
 	 *
 	 * @return array
 	 */
-	protected function getSettings(Composer $composer) {
+	protected function getSettings(Composer $composer)
+	{
 		$settings = array(
 			'targetDir' => 'vendor/nodejs/nodejs',
 			'forceLocal' => false,
@@ -107,6 +99,145 @@ class Plugin implements PluginInterface, EventSubscriberInterface
 		}
 
 		return $settings;
+	}
+
+	/**
+	 * Writes message when verbose mode is enabled.
+	 *
+	 * @param string $message Message to write
+	 * @param IOInterface $io InputOutput interface
+	 */
+	private function verboseWrite($message, IOInterface $io)
+	{
+		if ($io->isVerbose()) {
+			$io->write($message);
+		}
+	}
+
+	/**
+	 * Execute command
+	 *
+	 * @param string $cmd Command to execute
+	 * @param string $path Path where to run
+	 * @param IOInterface $io InputOutput interface
+	 */
+	protected function exec($cmd, $path, IOInterface $io)
+	{
+		$process = new Process($cmd, $path);
+		$process->mustRun(function ($type, $buffer) use ($io) {
+			if (Process::ERR === $type) {
+				$io->writeError($buffer);
+			} else {
+				$io->write($buffer);
+			}
+		});
+	}
+
+	/**
+	 * Create bin scripts
+	 *
+	 * @param string $binDir vendor/bin dir
+	 * @param string $fullTargetDir NodeJS instalation path
+	 * @param bool $isLocal Is local
+	 */
+	protected function createBinScripts($binDir, $fullTargetDir, $isLocal)
+	{
+		if (!Environment::isWindows()) {
+			$this->writeShLinkScript($binDir, $fullTargetDir, 'yarn', 'yarn', $isLocal);
+			$this->writeShLinkScript($binDir, $fullTargetDir, 'yarnpkg', 'yarnpkg', $isLocal);
+			chmod($binDir . '/yarn', 0755);
+			chmod($binDir . '/yarnpkg', 0755);
+		} else {
+			$this->writeBatLinkScript($binDir, $fullTargetDir, 'yarn.bat', 'yarn.cmd', $isLocal);
+			$this->writeBatLinkScript($binDir, $fullTargetDir, 'yarnpkg.bat', 'yarnpkg.cmd', $isLocal);
+		}
+	}
+
+	/**
+	 * Write .sh script
+	 *
+	 * @param string $binPath vendor/bin path
+	 * @param string $fullTargetDir Where script that must be executed is locate
+	 * @param string $script_filename Script filename where to write contents
+	 * @param string $exec_filename Filename to execute
+	 * @param bool $isLocal Is local?
+	 *
+	 * @return bool
+	 */
+	protected function writeShLinkScript($binPath, $fullTargetDir, $script_filename, $exec_filename, $isLocal)
+	{
+		return $this->writeBinScript(
+			$binPath,
+			$script_filename,
+			array(
+				'#!/usr/bin/env bash',
+				$this->getFilePathForScript(
+					$binPath,
+					$fullTargetDir,
+					$exec_filename,
+					$isLocal
+				) . ' $@'
+			)
+		);
+	}
+
+	/**
+	 * Writes bin script
+	 *
+	 * @param $path
+	 * @param $filename
+	 * @param $commands
+	 */
+	protected function writeBinScript($path, $filename, array $commands)
+	{
+		return file_put_contents($path . DIRECTORY_SEPARATOR . $filename, implode(PHP_EOL, $commands), LOCK_EX) !== false;
+	}
+
+	/**
+	 * Gets filepath for script
+	 *
+	 * @param string $binPath Bin path
+	 * @param string $fullTargetDir Traget dir
+	 * @param string $filename Filename
+	 * @param bool $isLocal Is local?
+	 *
+	 * @return string
+	 */
+	protected function getFilePathForScript($binPath, $fullTargetDir, $filename, $isLocal)
+	{
+		if (!$isLocal) {
+			return $fullTargetDir . DIRECTORY_SEPARATOR . $filename;
+		}
+		$converter = new Converter($fullTargetDir, realpath($binPath));
+		return $converter->convert($filename);
+	}
+
+	/**
+	 * Write .bat script
+	 *
+	 * @param string $binPath vendor/bin path
+	 * @param string $fullTargetDir Where script that must be executed is locate
+	 * @param string $script_filename Script filename where to write contents
+	 * @param string $exec_filename Filename to execute
+	 * @param bool $isLocal Is local?
+	 *
+	 * @return bool
+	 */
+	protected function writeBatLinkScript($binPath, $fullTargetDir, $script_filename, $exec_filename, $isLocal)
+	{
+		return $this->writeBinScript(
+			$binPath,
+			$script_filename,
+			array(
+				'@ECHO OFF',
+				$this->getFilePathForScript(
+					$binPath,
+					$fullTargetDir,
+					$exec_filename,
+					$isLocal
+				) . ' %*'
+			)
+		);
 	}
 
 }
