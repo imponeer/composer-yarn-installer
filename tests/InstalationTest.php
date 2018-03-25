f035a79585b2e0b5457d5e47369f0dc69e48f886
Raimondas Rimkevičius <github@mekdrop.name>
2018-03-25 19:38:01 +0300

    chmod

--- tests/InstalationTest.php
+++ tests/InstalationTest.php
@@ -106,7 +106,7 @@ class InstalationTest extends TestCase
 		$this->dirs[0] = $this->generateRandomFolder();
 		$this->dirs[1] = $this->generateRandomFolder();
 		$filesystem = new Filesystem();
-		array_walk($this->dirs, array($filesystem, 'mkdir'));
+		array_walk($this->dirs, array($filesystem, 'mkdir'), 0777);
 		file_put_contents(
 			$this->dirs[0] . DIRECTORY_SEPARATOR . 'composer.json',
 			$this->generate_composer_json(true),
